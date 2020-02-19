4834fb5a859f4088fa8aa303d15a28dbfbc2ce26
Sander van Wickeren <sandervanwickeren@gmail.com>
2020-02-19 15:03:47 +0100

    Formatted data for year distribution per edition

--- infovis/main.py
+++ infovis/main.py
@@ -1,5 +1,6 @@
 from flask import Flask
-from flask import send_file
+from flask import send_file, jsonify
+import json, os
 
 app = Flask(__name__, static_folder="public/dist", static_url_path="/",)
 
@@ -8,3 +9,9 @@ app = Flask(__name__, static_folder="public/dist", static_url_path="/",)
 def route_index():
     """This route exists purely to serve index.html to the root"""
     return send_file("public/dist/index.html")
+
+
+@app.route("/music_age", methods=["GET", "POST"])
+def route_music_age():
+    with open(os.path.join(os.getcwd(), "data", "year_distribution_per_edition.json")) as jsonfile:
+        return jsonify(json.load(jsonfile))
