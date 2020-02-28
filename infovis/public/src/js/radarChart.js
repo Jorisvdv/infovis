61d856f3b5a133b948a0aa461c28b3dab602ba70
sandervwickeren <sander@bit-students.com>
2020-02-28 16:02:29 +0100

    Added animations to the radarchart

--- infovis/public/src/js/radarChart.js
+++ infovis/public/src/js/radarChart.js
@@ -4,13 +4,12 @@ function fakeData() {
 	let data = [];
 	let features = ["A","B","C","D","E","F"];
 	//generate the data
-	for (var i = 0; i < 3; i++){
+	for (var i = 0; i < 10; i++){
 	    var point = {}
 	    //each feature will be a random number from 0 - 1
 	    features.forEach(f => point[f] = Math.random() );
 	    data.push(point);
 	}
-	console.log(data)
 	return data
 }
 
@@ -46,7 +45,20 @@ function realData() {
 	"speechiness":0.027,"tempo":147.125,"valence":0.609,
 	"genre":["album rock","classic rock","country rock",
 	"folk rock","heartland rock","mellow gold","rock",
-	"singer-songwriter","soft rock","yacht rock"]}]
+	"singer-songwriter","soft rock","yacht rock"]}, {"titel":"Child In Time",
+	"artiest":"Deep Purple","jaar":1972,"id":"5TrfusgKC1BYic5LaWCoKm",
+	"1999":3.0,"2000":2.0,"2001":2.0,"2002":2.0,"2003":3.0,"2004":3.0,"2005":4.0,
+	"2006":4.0,"2007":4.0,"2008":4.0,"2009":5.0,"2010":4.0,"2011":3.0,"2012":4.0,
+	"2013":4.0,"2014":4.0,"2015":8.0,"2016":5.0,"2017":5.0,"2018":6.0,"2019":12.0,
+	"artist_id":"spotify:artist:568ZhdwyaiCyOGJRtNYhWf","album_id":"spotify:album:3llL1qaL2RvtyQAthAuRFS",
+	"image_640":"https:\/\/i.scdn.co\/image\/ab67616d0000b273c093f46ed58856808fef4c8d",
+	"image_64":"https:\/\/i.scdn.co\/image\/ab67616d00004851c093f46ed58856808fef4c8d",
+	"popularity":44,"duration":620373,
+	"preview_url":"https:\/\/p.scdn.co\/mp3-preview\/9c5b8bf85a3d429ab3b81aae97f480497eefe47e?cid=1a316693f32b4bd4acee870703d5338b",
+	"acousticness":0.17,"danceability":0.338,"energy":0.461,"instrumentalness":0.564,
+	"liveness":0.0927,"loudness":-10.74,"speechiness":0.0396,"tempo":126.698,
+	"valence":0.351,"genre":["album rock","blues rock","classic rock",
+	"hard rock","metal","psychedelic rock","rock"]}]
 
 	let features = ["acousticness", "danceability", "energy",
 	 "instrumentalness", "liveness", "valence", "speechiness"];
@@ -146,9 +158,6 @@ function drawRadar(location, data, size=1, features=[]) {
 
 
 	// Plotting the data
-	// let line = d3.line()
-	// 	.x(d => parseInt(d.x))
-	// 	.y(d => parseInt(d.y));
 	let colors = ["darkorange", "gray", "navy", "green"];
 
 
@@ -162,14 +171,68 @@ function drawRadar(location, data, size=1, features=[]) {
 	    return coordinates;
 	}
 
-	let t = d3.transition().delay(200).duration(700);
 
-	 for (var i = 0; i < data.length; i ++){
+
+	let tempPoints = [[123, 30], [12, 123], [123, 123], [0,0], [0,0], [0,0]];
+	let tempPoints2 = [[{x: 123, y: 30}, {x: 12, y:123}, {x:123, y:123},
+					  {x:0, y:0}, {x:0, y:0}, {x:0, y:0}],
+					  [{x: 300, y: 200}, {x: 230, y:60}, {x:100, y:140}]]
+	let tempPoints3 = [[{x: 300, y: 200}, {x: 230, y:60}, {x:100, y:140}]]
+	let tempPoints4 = [[{x: 123, y: 30}, {x: 12, y:123}, {x:123, y:123},
+					  {x:0, y:0}, {x:0, y:0}, {x:0, y:0}]]
+
+
+	    // Rendering of the graph
+    const renderRadarChart = (selection, linePaths) => {
+		const line = d3.line()
+		.x(d => d.x)
+		.y(d => d.y);
+
+		const startLine = d3.line()
+			.x(d => d.x / 10)
+			.y(d => d.y / 10);
+
+		const stopLine = d3.line()
+			.x(d => width / 2)
+			.y(d => width / 2);
+
+		// Get all the paths:
+		const paths = selection.selectAll('path').data(linePaths);
+
+		// Add all paths to svg
+		paths.enter()
+			.append("path")
+			.attr("opacity", 0.5)
+			.attr('d', function(d) {return stopLine(d) + 'Z'})
+
+			// Causes the new data to merge with the old data
+			.merge(paths)
+			.transition().duration(1000)
+			.attr('d', function(d) {return line(d) + 'Z'});
+
+		// Remove paths that are no longer in the data.
+		paths.exit()
+			.transition().duration(1000)
+				.attr('d', function(d) {return stopLine(d) + 'Z'})
+			.remove()
+	}
+
+	function timedExecution(i) {
+		setTimeout(() => {
 		    let d = data[i];
 		    let color = colors[i];
 		    let coordinates = getPathCoordinates(d);
-		    console.log(coordinates)
+			renderRadarChart(svg, [coordinates])
+		}, i * 2000)
+	}
 
+	
+	 for (let i=0; i<data.length; i++){
+ 		timedExecution(i);
+
+ 		if (i === (data.length - 1)) {
+ 			location.reload(true)
+ 		}
 		    //draw the path element
 		    // svg.append("path")
 			   //  .datum(coordinates)
@@ -183,95 +246,6 @@ function drawRadar(location, data, size=1, features=[]) {
 			   //  	.attr("stroke", color)
 				  //   .attr("fill", color)
 	}
-
-	let tempPoints = [[123, 30], [12, 123], [123, 123], [0,0], [0,0], [0,0]];
-	let tempPoints2 = [[{x: 123, y: 30}, {x: 12, y:123}, {x:123, y:123},
-					  {x:0, y:0}, {x:0, y:0}, {x:0, y:0}],
-					  [{x: 300, y: 200}, {x: 230, y:60}, {x:100, y:140}]]
-	let tempPoints3 = [[{x: 300, y: 200}, {x: 230, y:60}, {x:100, y:140}]]
-	let tempPoints4 = [[{x: 123, y: 30}, {x: 12, y:123}, {x:123, y:123},
-					  {x:0, y:0}, {x:0, y:0}, {x:0, y:0}]]
-	// var line = d3.line()
-	// 	.x(d => d.x)
-	// 	.y(d => d.y);
-
-	// function render() {
-	// 	svg.selectAll('path') 
-	// 		.data(tempPoints2)
-	// 		.enter()
-	// 		.append("path")
-	// 		.attr('d', function(d) {return line(d) + 'Z'});
-
-	// 	//tempPoints2.pop()
-
-	// 	svg.selectAll('path').data(tempPoints2).exit().remove()
-
-	//     // let path = svg.selectAll('path').data([[1, 3], [2, 7], [3, 2], [5, 2]])
-	//     // path.attr('d', function(d){ console.log(d); return line(d)})
-	//     //     .style('stroke-width', 1)
-	//     //     .style('stroke', 'steelblue');
-	//     // path.enter().append('svg:path').attr('d', function(d){return line(d)})
-	//     //     .style('stroke-width', 1)
-	//     //     .style('stroke', 'steelblue');
-	//     // path.exit().remove()
-	// }
-
-	//     render()
-
-	    // Rendering of the graph
-    const renderRadarChart = (selection, linePaths) => {
-    	console.log("called: ", linePaths)
-		const line = d3.line()
-		.x(d => d.x)
-		.y(d => d.y);
-
-		// Get all the paths:
-		const paths = selection.selectAll('path').data(linePaths);
-
-		// Add all paths to svg
-		paths.enter()
-			.append("path")
-			.attr("opacity", 0.5)
-
-			// Causes the new data to merge with the old data
-			.merge(paths)
-			.attr('d', function(d) {return line(d) + 'Z'});
-
-		// Remove paths that are no longer in the data.
-		paths.exit().remove()
-	}
-
-	// Removing an item
-	renderRadarChart(svg, tempPoints2)
-	setTimeout(() => {
-		tempPoints2.pop()
-		renderRadarChart(svg, tempPoints2)
-	}, 1000)
-
-	// Adding an item
-	setTimeout(() => {
-
-		tempPoints2.push(tempPoints3[0])
-		renderRadarChart(svg, tempPoints3)
-	}, 2000)
-
-	
-
-	// let color = colors[i];
-	// svg.selectAll("path")
-	// .data([getPathCoordinates[data[i]]])
-	// 	.enter()
-	// 	.append('path')
-	// 	.attr("stroke", color)
-	//     .attr("stroke-opacity", 1)
-	//     .attr("stroke", color)
-	//     .transition(t)
-	//     	.attr("d", line)
-	//     	.attr("stroke-width", 3)
-	//     	.attr("opacity", 0.5)
-	//     	.attr("stroke", color)
-	// 	    .attr("fill", color)
-
 }
 
-drawRadar("#radarchart", [realData()[0]], 1);
+drawRadar("#radarchart", realData(), 1);
