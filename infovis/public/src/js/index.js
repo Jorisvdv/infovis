60fce26fd2e461655305511268287805b327fa76
stijnveken <19489411+stijnveken@users.noreply.github.com>
2020-03-14 10:56:37 +0100

    Features over time component (#2)
    * WIP
    
    * added feature over time component with selectable features and transition between selected features. \n TODO: fix colors of lines of selected features. Something goes wrong at 'linechart.js' line 81.
    
    * wip new structure
    
    * created backup file for line_chart.js called line_chart_old.js
    
    * fixed placement of line chart component
    
    * Combined json for scatterplot
    
    * Create lineplot object and rename release year
    
    * fix lines, colors
    
    * added tooltip functionality in linechart component
    
    * finished line chart component, added onClick event for selection of year
    
    * renamed function _getMouseOverYear to _getClosestYearToMouse
    
    * added colored labels
    
    Co-authored-by: Guido <guido.v@bit-students.com>
    Co-authored-by: Joris van der Vorst <joris@jvandervorst.nl>

--- infovis/public/src/js/index.js
+++ infovis/public/src/js/index.js
@@ -1,6 +1,26 @@
-import * as d3 from "d3"
+import lineChartData from "../../../../data/data/lineplot_object"
 
-const body = d3.select("body");
-body
-    .append("h1")
-    .text("Hello from d3, update")
+import LineChart from "./line_chart";
+
+// only show the features selected
+function updateLineChart() {
+    const data = Array.from(lineChartData).filter(item => {
+        const checkbox = document.getElementById(item.key)
+        return checkbox ? checkbox.checked : false;
+    })
+
+    lineChart.update(data)
+}
+
+Array.from(document.getElementsByClassName("line-chart-inputs")[0].children).forEach(element => {
+    element.children[0].addEventListener("click", updateLineChart)
+})
+
+const onClick = (year) => {
+    console.log(year);
+}
+
+const lineChart = new LineChart("#line-chart", onClick);
+lineChart.init()
+updateLineChart()
+
