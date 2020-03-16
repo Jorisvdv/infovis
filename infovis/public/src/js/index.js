c23bfe88649f5cb23e9fcf1fa4a6908b02adb75f
Joris van der Vorst <joris@jvandervorst.nl>
2020-03-16 16:27:35 +0100

    Linechart with genres

--- infovis/public/src/js/index.js
+++ infovis/public/src/js/index.js
@@ -2,7 +2,7 @@ import genreData from "./../../../../data/data/scatterplot.json"
 import RadarChart from "./radarChart"
 import ScatterPlot from "./scatterPlot"
 import LineChart from "./line_chart";
-import lineChartData from "../../../../data/data/lineplot_object"
+import lineChartData from "../../../../data/data/lineplot_genre_object"
 import SeatingChart from "./seatingChart";
 import seatingData from "../../../../data/song_data_20200304.json"
 
